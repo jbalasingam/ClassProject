e04c6fd110884ac85e6a3d6756a9177d33767921
jbalasingam <mervinb@gmail.com>
2018-05-26 23:28:03 -0400

    initial site files

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -24,41 +24,83 @@ var YTORIO  = {
     id: "YTORIO",
     city: "Rio de Janeiro",
     countryID : "BR",
-    lattitude : -22,
-    longitude : -42.5,
+    lattitude : -22.9068,
+    longitude : -43.1729,
 }   
 
+var tempTimeSeries = [];
+var precipTimeSeries = [];
+var humidTimeSeries = [];
 
 var destinations = [YTODEN, YTOSFO, YTORIO];   
 
 
-var startDate = parseInt((new Date('2012-08-10').getTime() / 1000).toFixed(0));
-var endDate = parseInt((new Date('2012-12-10').getTime() / 1000).toFixed(0));
+var startDate = parseInt((new Date('2012-08-30').getTime() / 1000).toFixed(0));
+var endDate = parseInt((new Date('2012-08-31').getTime() / 1000).toFixed(0));
 
 
 
 $(".submit").on("click", function() {
     //get the id of the current destination selected
-
     var ID = $('.option:selected').attr("id")
+    //clear the arrays
+    tempTimeSeries = [];
+    precipTimeSeries = [];
+    humidTimeSeries = [];
 
+    //make sure that the user selects a destion before passing on a value to city
     if (ID != "NULL"){
-
-        var city = destinations[ID].city;
-        console.log(city)
         $(".selectDestWarning").hide();
+        var city = destinations[ID].city;
+        var lattitude = destinations[ID].lattitude;
+        var longitude = destinations[ID].longitude;
+
+        console.log(city);
+        console.log(lattitude);
+        console.log(longitude);
+
+        var today = new Date('2017-08-27');
+        var tomorrow = today.setDate(today.getDate() + 1)
+        tomorrow = tomorrow/1000;
         
+        var i;
+        for (i = 0; i < 3; i++) { 
+
+            var tomorrow = today.setDate(today.getDate() + i);
+            tomorrow = tomorrow/1000;
+            
+            console.log(tomorrow);
+
+            $.getJSON(proxy+"https://api.darksky.net/forecast/"+ APIKEY  + lattitude + "," + longitude + "," + tomorrow,function(snapshot){
+                // var currentTemp = json.main.temp - AbsZero;
+                console.log(snapshot);
+                tempTimeSeries.push(snapshot.currently.temperature);
+                precipTimeSeries.push(snapshot.currently.precipType);
+                humidTimeSeries.push(snapshot.currently.humidity);
+             });//end get json
+
+        }
+        
+        console.log(tempTimeSeries);
+        console.log(precipTimeSeries);
+        console.log(humidTimeSeries);
+
+
+
+
+
+        // $.getJSON(proxy+"https://api.darksky.net/forecast/"+ APIKEY  + lattitude + "," + longitude + "," + startDate,function(snapshot){
+        //         // var currentTemp = json.main.temp - AbsZero;
+        //         console.log(snapshot);
+                
+        // });//end get json
+
     } else {
 
         $(".selectDestWarning").show();
         
-    }
-        
+    }//end if statement checking for NULL
+
     
-    // $.getJSON(proxy+"https://api.darksky.net/forecast/"+ APIKEY  + lattitude + "," + longitude + "," + startDate,function(snapshot){
-    //     // var currentTemp = json.main.temp - AbsZero;
-    //     console.log(snapshot.hourly.summary);
-        
-    // });
 
 });//end on submit-on-click function
