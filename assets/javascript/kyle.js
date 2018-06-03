b431d90c0ccaf6dc7ee6e11000f8415e740217a2
jbalasingam <mervinb@gmail.com>
2018-06-03 19:52:18 -0400

    initial site files

--- assets/javascript/kyle.js
+++ assets/javascript/kyle.js
@@ -1,4 +1,10 @@
 
+$(".selectDestWarning").hide();
+$(".tempTitle").hide();
+$(".tempTitle").hide();
+$("#depdisp").hide();
+$("#retdisp").hide();
+
 var Rio = "Rio de Janeiro is a huge seaside city in Brazil, famed for its Copacabana and Ipanema beaches, 38m Christ the Redeemer statue atop Mount Corcovado and for Sugarloaf Mountain, a granite peak with cable cars to its summit. The city is also known for its sprawling favelas (shanty towns). Its raucous Carnaval festival, featuring parade floats, flamboyant costumes and samba dancers, is considered the world’s largest.";
 var Denver = "Denver, the capital of Colorado, is an American metropolis dating to the Old West era. Larimer Square, the city’s oldest block, features landmark 19th-century buildings. Museums include the Denver Art Museum, an ultramodern complex known for its collection of indigenous works, and the mansion of famed Titanic survivor Molly Brown. Denver is also a jumping-off point for ski resorts in the nearby Rocky Mountains.";
 var Sanfran = "San Francisco, in northern California, is a hilly city on the tip of a peninsula surrounded by the Pacific Ocean and San Francisco Bay. It's known for its year-round fog, iconic Golden Gate Bridge, cable cars and colorful Victorian houses. The Financial District's Transamerica Pyramid is its most distinctive skyscraper. In the bay sits Alcatraz Island, site of the notorious former prison.";
@@ -10,8 +16,7 @@ img2.src = "./assets/images/denver.jpg";
 var img3 = document.createElement("img");
 img3.src = "./assets/images/san.jpg";
 
-$(".selectDestWarning").hide();
-$(".tempTitle").hide();
+
 var Constant = 32;
 var fraction = 5/9;
 
@@ -79,6 +84,10 @@ var select;
 $("#down_form").on('submit', function(e){
 //kyles code
     e.preventDefault();
+
+    $("#depdisp").show();
+    $("#retdisp").show();
+
     select = $('.form-control option:checked').val();
     
    if(select == 0){
